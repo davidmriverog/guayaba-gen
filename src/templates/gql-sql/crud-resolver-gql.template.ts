246ec4b29e41a8955a127128068d9887c3c1efc9
David Rivero <davidmriverog@gmail.com>
2021-08-18 11:00:10 -0300

    🔥 actualizacion guayaba..

--- src/templates/gql-sql/crud-resolver-gql.template.ts
+++ src/templates/gql-sql/crud-resolver-gql.template.ts
@@ -9,8 +9,8 @@ export const CrudResolverGQLTemplate = (entityName: string) => {
 
   return `
   import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-  import { UseGuards } from '@nestjs/common';
-  // import { GqlAuthGuard } from '../../../graphql/auth/guard/ggl-auth.guard';
+
+  import { Protected } from "../../../core/lib";
   import { FilterCriteriaInfo } from '../../../core/lib/tables/criteria.table';
 
   import { ${singular(pascalEntity)}Service } from './${customSingular(entityName)}.service';
@@ -24,37 +24,37 @@ export const CrudResolverGQLTemplate = (entityName: string) => {
     constructor(private readonly engineService: ${singular(pascalEntity)}Service) { }
 
     @Query(() => ${singular(pascalEntity)}ListPageInfoResponse)
-    // @UseGuards(GqlAuthGuard)
+    @Protected()
     async ${customSingularPascalCamelCase(pascalEntity)}ListPage(@Args('${customSingularPascalCamelCase(pascalEntity)}Criteria', { type: () => FilterCriteriaInfo }) ${customSingularPascalCamelCase(pascalEntity)}Criteria: FilterCriteriaInfo) {
       return await this.engineService.listPage(${customSingularPascalCamelCase(pascalEntity)}Criteria);
     }
 
     @Query(() => [${singular(pascalEntity)}], { name: '${customSingularPascalCamelCase(entityName)}FindAll' })
-    // @UseGuards(GqlAuthGuard)
+    @Protected()
     async findAll() {
       return await this.engineService.findAll();
     }
 
     @Query(() => ${singular(pascalEntity)}, { name: '${customSingularPascalCamelCase(entityName)}' })
-    // @UseGuards(GqlAuthGuard)
+    @Protected()
     async findOne(@Args('id', { type: () => Int }) id: number) {
       return await this.engineService.findOne(id);
     }
 
     @Mutation(() => ${singular(pascalEntity)})
-    // @UseGuards(GqlAuthGuard)
+    @Protected()
     async create${singular(pascalEntity)}(@Args('${customSingularPascalCamelCase(entityName)}InputDto') ${customSingularPascalCamelCase(entityName)}InputDto: ${singular(pascalEntity)}InputDto) {
       return await this.engineService.create(${customSingularPascalCamelCase(entityName)}InputDto);
     }
 
     @Mutation(() => ${singular(pascalEntity)})
-    // @UseGuards(GqlAuthGuard)
+    @Protected()
     async update${singular(pascalEntity)}(@Args('id', { type: () => Int }) id: number, @Args('${customSingularPascalCamelCase(entityName)}InputDto') ${customSingularPascalCamelCase(entityName)}InputDto: ${singular(pascalEntity)}InputDto) {
       return await this.engineService.update(id, ${customSingularPascalCamelCase(entityName)}InputDto);
     }
 
     @Mutation(() => ${singular(pascalEntity)})
-    // @UseGuards(GqlAuthGuard)
+    @Protected()
     async remove${singular(pascalEntity)}(@Args('id', { type: () => Int }) id: number) {
       return await this.engineService.remove(id);
     }
