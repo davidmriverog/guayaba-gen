73ebfea846b858f6cdd333d48811d75f0d71288c
David Rivero <davidmriverog@gmail.com>
2021-10-04 09:33:41 -0300

    fix

--- src/core/generate/gql-sql/crud-entity-gql.generate.ts
+++ src/core/generate/gql-sql/crud-entity-gql.generate.ts
@@ -1,7 +1,8 @@
 import * as fs from "fs"
 import * as path from "path"
 import { singular } from "pluralize"
-import { CrudEntityGQLNoPrefixTemplate } from "../../../templates/gql-sql/crud-entity-gql.template"
+
+import { CrudEntityGQLTemplate } from "../../../templates/gql-sql/crud-entity-gql.template"
 
 import { writeFile } from '../../utils/writer-files.util'
 
@@ -13,7 +14,7 @@ export const GenerateCrudEntityGQLModule = async (rootName: string, entityName: 
 
   const filePath = path.resolve(filePathEntity, `${singular(entityName)}.entity.ts`)
 
-  const rendered = CrudEntityGQLNoPrefixTemplate(rootName, entityName)
+  const rendered = CrudEntityGQLTemplate(rootName, entityName)
 
   await writeFile(rendered, filePath)
 }
