a4939c3c5e2ddbeded5709ca48c46b990eedc6f2
David Rivero <davidmriverog@gmail.com>
2022-07-05 17:57:05 -0300

    🐛

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,27 +2,6 @@ export * from "./config/param.config"
 
 export * from "./consts/nest-value-option.const"
 
-export * from "./generate/gql-nsql/root-module.generate"
-export * from "./generate/gql-nsql/update-root-module.generate"
-export * from "./generate/gql-nsql/update-root-hql-module.generate"
-
-export * from "./generate/gql-nsql/crud-entity.generate"
-export * from "./generate/gql-nsql/crud-resolver.generate"
-export * from "./generate/gql-nsql/crud-module-entity.generate"
-export * from "./generate/gql-nsql/crud-service.generate"
-
-export * from "./generate/api-sql/api-crud-module.generate"
-export * from "./generate/api-sql/api-entity.generate"
-export * from "./generate/api-sql/api-dto-module.generate"
-export * from "./generate/api-sql/api-entity-service.generate"
-export * from "./generate/api-sql/api-entity-module.generate"
-
-// NSQL API REST GENERATE
-export * from "./generate/api-nsql/api-nsql-entity.generate"
-export * from "./generate/api-nsql/api-nsql-controller.generate"
-export * from "./generate/api-nsql/api-update-root-template-nsql.generate"
-export * from "./generate/api-nsql/api-update-root-api-nsql.generate"
-
 export * from "./generate/gql-sql/crud-dto-gql.generate"
 export * from "./generate/gql-sql/crud-entity-gql.generate"
 export * from "./generate/gql-sql/crud-module-entity-gql.generate"
@@ -38,8 +17,8 @@ export * from "./utils/convert-singular.util"
 
 export * from "./images/main-graphql"
 
-
 // BUILD GENERATED GUAYABA PRO
+
 export * from "./build-generate/common/crud-dto-graphql.common"
 export * from "./build-generate/common/crud-module-entity-graphql.common"
 export * from "./build-generate/common/crud-module-entity-restapi.common"
